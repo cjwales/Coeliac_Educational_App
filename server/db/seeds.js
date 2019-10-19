1d6c1b50c3d2aa02d600d1b9f3481179b2b7248e
sreenandini <mallelasreenandini2@gmail.com>
2019-10-19 12:06:03 +0100

    added lat and lan in the seeds file

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -9,6 +9,8 @@ db.restaurants.insertMany([
     reviews: ["food was so delicious"],
     location: "89 Candleriggs, Merchant City, Glasgow ",
     postcode:"G1 1NP",
+    longitude: -4.24578,
+    latitude: 55.85883,
     phone: 01415532585
   },
   {
@@ -18,6 +20,8 @@ db.restaurants.insertMany([
     reviews: [],
     location: "89 St Vincent St, Glasgow ",
     postcode:"G2 5TF",
+    longitude:-4.256502,
+    latitude:55.861253,
     phone: 01412489359
   },
   {
@@ -26,7 +30,9 @@ db.restaurants.insertMany([
     cuisine: "Health food",
     reviews: [],
     location: "130 W Regent St, Glasgow ",
-    postcode:"G2 5TF",
+    postcode:"G2 2RQ",
+    longitude:-4.259998,
+    latitude:55.863679,
     phone: 01412120861
   },
   {
@@ -36,6 +42,8 @@ db.restaurants.insertMany([
     reviews: [],
     location: "90 St Vincent St, Glasgow ",
     postcode:"G2 5UB",
+    longitude:-4.255589,
+    latitude:55.861536,
     phone: 01412378020
   },
   {
@@ -45,6 +53,8 @@ db.restaurants.insertMany([
     reviews: [],
     location: "59 Mitchell St, Glasgow ",
     postcode:"G1 3LN",
+    longitude:-4.256082,
+    latitude: 55.859567,
     phone: 01412482240
   },
   {
@@ -54,6 +64,8 @@ db.restaurants.insertMany([
     reviews: [],
     location: "133 Wellington St, Glasgow ",
     postcode:"G2 2XD",
+    longitude: -4.259433,
+    latitude: 55.863883,
     phone: 01412482228
   },
   {
@@ -63,6 +75,8 @@ db.restaurants.insertMany([
     reviews: [],
     location: "257 W Campbell St, Glasgow ",
     postcode:"G2 4TT",
+    longitude:-4.260857,
+    latitude: 55.864148,
     phone: 01412216000
   },
   {
@@ -72,7 +86,8 @@ db.restaurants.insertMany([
     reviews: [],
     location: " 447 Great Western Rd, Glasgow ",
     postcode:"G12 8HH",
-
+    longitude:-4.280905,
+    latitude: 55.874895,
     phone: 01413340686
   },
 ]);
